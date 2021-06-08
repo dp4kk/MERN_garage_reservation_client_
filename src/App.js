556a0157c15f61190ab21266ebda3b932c66a197
Deepak kushwaha <kushwahadeepak797@gmail.com>
2021-06-08 17:21:05 +0545

    Garage app first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import FormicForm from "./components/FormicForm";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import DataContext from "./contexts/DataContext";
+import { Home } from "./pages/Home";
+import Services from "./pages/Services";
+import Register from './pages/Register'
+import Login from "./pages/Login";
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import Footer from "./components/Footer";
+import {PrivateRoute} from './contexts/PrivateRoute'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <DataContext>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <React.Fragment>
+              <Navbar />
+              <Sidebar />
+              <Route exact path="/" component={Home} />
+              <Route path="/services" component={Services} />
+              <PrivateRoute path="/bookservice" component={FormicForm} />
+            </React.Fragment>
+          </Switch>
+          <Footer />
+        </DataContext>
+      </Router>
+    </React.Fragment>
   );
 }
 
 export default App;
+
